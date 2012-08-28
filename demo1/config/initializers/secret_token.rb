d5c5a7c11c2f4a0795b4fec4363166434dbd8e11
Fei <yufei.flyingfish@gmail.com>
2012-08-28 21:11:45 +0800

    Hello World Project Creation of Ruby

---
+++ demo1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo1::Application.config.secret_token = 'c7187646943ed0ce36fa645495ba455c2c6225b570bd9cff655203b14684309af50aa078a8ed824bb531cc213b94c8f6e90b02de59da4e6045c8505554bff01f'
